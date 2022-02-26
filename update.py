596bc220d425e8a864694e405b5581a8097e83ac
https://github.com/magenta1223 <githubEmailAddress>
2022-02-26 21:20:19 +0900

    Changed :  update.py

--- update.py
+++ update.py
@@ -4,18 +4,18 @@ import os
 
 def main():
     types = [t.replace('.json', '') for t in os.listdir('task/configs/')]
-    existing_types = [ et.type for et in EstimatorType.objects.all()]
-    types = list( set(types) - set(existing_types) )
+    #existing_types = [ et.type for et in EstimatorType.objects.all()]
+    #types = list( set(types) - set(existing_types) )
 
-    if len(types):
-        for t in types:
-            kwargs, name = config_parser(t)
-            e = EstimatorType(type = t, name = name, kwargs = kwargs)
-            e.save()
+    EstimatorType.objects.all().delete()
+
+    for t in types:
+        kwargs, name = config_parser(t)
+        e = EstimatorType(type = t, name = name, kwargs = kwargs)
+        e.save()
+
+    print('Updated')
     
-        print('Updated')
-    
-    else:
-        print('Already up to date')
+
 
 main()
